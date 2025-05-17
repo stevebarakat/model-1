2926ea84cc48f7acc65353ee75d382fdeb8c9cbe
stevebarakat <stevebarakat@gmail.com>
2025-05-17 03:15:06 -0400

    ci

--- src/components/Noise/Noise.tsx
+++ src/components/Noise/Noise.tsx
@@ -28,47 +28,53 @@ function Noise({
   onSyncChange,
 }: NoiseProps) {
   return (
-    <div className={styles.column}>
-      <div className="row">
-        <Knob
-          value={volume}
-          min={0}
-          max={1}
-          step={0.01}
-          label="Volume"
-          onChange={onVolumeChange}
-        />
-        <Switch
-          checked={type === "pink"}
-          onCheckedChange={(checked) =>
-            onTypeChange(checked ? "pink" : "white")
-          }
-          label={type === "pink" ? "Pink" : "White"}
-        />
-      </div>
-      <span className={styles.horizontalIndent}></span>
-      <div className="row">
-        <Knob
-          value={pan}
-          min={-1}
-          max={1}
-          step={0.01}
-          label="Pan"
-          onChange={onPanChange}
-        />
-      </div>
-      <span className={styles.horizontalIndent}></span>
-      <div className="row">
-        <Knob
-          value={tone}
-          min={20}
-          max={20000}
-          step={1}
-          label="Freq"
-          onChange={onToneChange}
-          logarithmic={true}
-        />
-        <Switch checked={sync} onCheckedChange={onSyncChange} label="Sync" />
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div className={styles.column}>
+        <div className={styles.screwTopLeft} />
+        <div className={styles.screwTopRight} />
+        <div className={styles.screwBottomLeft} />
+        <div className={styles.screwBottomRight} />
+        <div className={styles.row}>
+          <Knob
+            value={volume}
+            min={0}
+            max={1}
+            step={0.01}
+            label="Volume"
+            onChange={onVolumeChange}
+          />
+          <Switch
+            checked={type === "pink"}
+            onCheckedChange={(checked) =>
+              onTypeChange(checked ? "pink" : "white")
+            }
+            label={type === "pink" ? "Pink" : "White"}
+          />
+        </div>
+        <span className={styles.spacer}></span>
+        <div className={styles.row}>
+          <Knob
+            value={pan}
+            min={-1}
+            max={1}
+            step={0.01}
+            label="Pan"
+            onChange={onPanChange}
+          />
+        </div>
+        <span className={styles.spacer}></span>
+        <div className={styles.row}>
+          <Knob
+            value={tone}
+            min={20}
+            max={20000}
+            step={1}
+            label="Freq"
+            onChange={onToneChange}
+            logarithmic={true}
+          />
+          <Switch checked={sync} onCheckedChange={onSyncChange} label="Sync" />
+        </div>
       </div>
       <span className={styles.horizontalIndent}></span>
     </div>
