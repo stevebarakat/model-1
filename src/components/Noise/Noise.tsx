94b545efb1663d9e62b037717f57b1cc80529091
stevebarakat <stevebarakat@gmail.com>
2025-05-17 21:37:14 -0400

    ci

--- src/components/Noise/Noise.tsx
+++ src/components/Noise/Noise.tsx
@@ -4,12 +4,12 @@ import styles from "./Noise.module.css";
 
 type NoiseProps = {
   volume: number;
-  pan: number;
+  pan?: number;
   type: "white" | "pink";
   tone: number;
   sync: boolean;
   onVolumeChange: (value: number) => void;
-  onPanChange: (value: number) => void;
+  onPanChange?: (value: number) => void;
   onTypeChange: (type: "white" | "pink") => void;
   onToneChange: (value: number) => void;
   onSyncChange: (sync: boolean) => void;
@@ -17,12 +17,10 @@ type NoiseProps = {
 
 function Noise({
   volume,
-  pan,
   type,
   tone,
   sync,
   onVolumeChange,
-  onPanChange,
   onTypeChange,
   onToneChange,
   onSyncChange,
@@ -34,52 +32,32 @@ function Noise({
         <div className={styles.screwTopRight} />
         <div className={styles.screwBottomLeft} />
         <div className={styles.screwBottomRight} />
-        <div className={styles.row}>
-          <Knob
-            value={volume}
-            min={0}
-            max={1}
-            step={0.01}
-            label="Noise"
-            onChange={onVolumeChange}
-          />
-          <Switch
-            checked={type === "pink"}
-            onCheckedChange={(checked) =>
-              onTypeChange(checked ? "pink" : "white")
-            }
-            topLabel="White"
-            bottomLabel="Pink"
-          />
-        </div>
-        <span className={styles.spacer}></span>
-        <div className={styles.row}>
-          <Knob
-            value={pan}
-            min={-1}
-            max={1}
-            step={0.01}
-            label="Pan"
-            onChange={onPanChange}
-          />
-        </div>
-        <span className={styles.spacer}></span>
-        <div className={styles.row}>
-          <Knob
-            value={tone}
-            min={440}
-            max={20000}
-            step={1}
-            label="Freq"
-            onChange={onToneChange}
-            logarithmic={true}
-          />
-          <Switch
-            checked={sync}
-            onCheckedChange={onSyncChange}
-            topLabel="Sync"
-          />
-        </div>
+        <Knob
+          value={volume}
+          min={0}
+          max={1}
+          step={0.01}
+          label="Noise"
+          onChange={onVolumeChange}
+        />
+        <Switch
+          checked={type === "pink"}
+          onCheckedChange={(checked) =>
+            onTypeChange(checked ? "pink" : "white")
+          }
+          label={type === "pink" ? "Pink" : "White"}
+        />
+
+        <Knob
+          value={tone}
+          min={440}
+          max={20000}
+          step={1}
+          label="Freq"
+          onChange={onToneChange}
+          logarithmic={true}
+        />
+        <Switch checked={sync} onCheckedChange={onSyncChange} label="Sync" />
       </div>
       <span className={styles.horizontalIndent}></span>
     </div>
--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -3,28 +3,14 @@ import styles from "./Switch.module.css";
 type SwitchProps = {
   checked: boolean;
   onCheckedChange: (checked: boolean) => void;
-  topLabel?: string;
-  bottomLabel?: string;
+  label?: string;
   className?: string;
 };
 
-function Switch({
-  checked,
-  onCheckedChange,
-  topLabel,
-  bottomLabel,
-  className,
-}: SwitchProps) {
-  const isPink = bottomLabel === "Pink";
-  const pinkStyle =
-    "linear-gradient(to left, hsl(320deg 70% 45% / 90%), hsl(320deg 70% 35% / 90%))";
-  const thumbStyle = {
-    background: checked && isPink ? pinkStyle : "",
-  };
-
+function Switch({ checked, onCheckedChange, label, className }: SwitchProps) {
   return (
     <div className={styles.switchContainer}>
-      {topLabel && <label className={styles.topLabel}>{topLabel}</label>}
+      {label && <label className={styles.topLabel}>{label}</label>}
       <button
         className={`${styles.switchRoot} ${className || ""}`}
         role="switch"
@@ -33,12 +19,8 @@ function Switch({
       >
         <div
           className={`${styles.switchThumb} ${checked ? styles.checked : ""}`}
-          style={thumbStyle}
         />
       </button>
-      {bottomLabel && (
-        <label className={styles.bottomLabel}>{bottomLabel}</label>
-      )}
     </div>
   );
 }
